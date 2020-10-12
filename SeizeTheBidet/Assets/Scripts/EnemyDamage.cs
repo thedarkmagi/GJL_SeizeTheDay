81b7216bc84799d3f9ef8801420f59d905430647
frogkaro <tirkaro@gmail.com>
2020-10-12 18:35:44 -0400

    90% of this is just the dialogue system lmao

---
+++ SeizeTheBidet/Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Melee")
+            Destroy(gameObject);// for now, apply a damage method here
+    }
+}
---
+++ SeizeTheBidet/Assets/Scripts/MeleeAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : MonoBehaviour
+{
+    //Variables;
+    Animator anim;
+
+    private void Start()
+    {
+
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            anim.SetBool("attacking", true);
+        else if (Input.GetButtonUp("Fire1"))
+            anim.SetBool("attacking", false);
+    }
+}
