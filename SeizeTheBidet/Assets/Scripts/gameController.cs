b81f409cec44451bbdc8e76372991db88bd9aa4a
Olly Mills <kinglslbob@gmail.com>
2020-10-14 17:40:20 +0100

    Piss Meter added

--- SeizeTheBidet/Assets/Scripts/gameController.cs
+++ SeizeTheBidet/Assets/Scripts/gameController.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class gameController : MonoBehaviour
 {
     public static gameController instance;
 
     public bool pre_pee=true;
+
+
+    public float Pee_maxTimeTillGameOver;
+    public Slider slider;
+    float Pee_curTimeTillGameOver;
+
     private void Awake()
     {
         if (instance != null)
@@ -23,12 +29,21 @@ public class gameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Pee_curTimeTillGameOver = 0;
+        slider.maxValue = Pee_maxTimeTillGameOver;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(pre_pee)
+        {
+            Pee_curTimeTillGameOver += Time.deltaTime;
+            slider.value = Pee_curTimeTillGameOver;
+            if(Pee_curTimeTillGameOver > Pee_maxTimeTillGameOver)
+            {
+                //GAME OVER 
+            }
+        }
     }
 }
