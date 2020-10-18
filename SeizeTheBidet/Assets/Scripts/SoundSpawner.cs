ae9c39ae6dd603c42d8d573ab6b050e05dece1c3
frogkaro <tirkaro@gmail.com>
2020-10-18 15:02:13 -0400

    i did not push a script OH NO

---
+++ SeizeTheBidet/Assets/Scripts/SoundSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundSpawner : MonoBehaviour
+{
+    [SerializeField] public GameObject customObject;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter (Collider Player)
+    {
+        customObject.SetActive(true);
+    }
+
+    void OnTriggerExit(Collider Player)
+    {
+        customObject.SetActive(false);
+    }
+
+}
